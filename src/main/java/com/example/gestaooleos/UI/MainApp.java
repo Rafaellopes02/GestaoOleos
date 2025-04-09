5a3efb9497338588897d4fb5b9bfe18e085c943e
Tiagomelo03 <tiagomelo@ipvc.pt>
2025-04-09 19:08:46 +0100

    Parte grafica

--- src/main/java/com/example/gestaooleos/UI/MainApp.java
+++ src/main/java/com/example/gestaooleos/UI/MainApp.java
@@ -2,6 +2,7 @@ package com.example.gestaooleos.UI;
 
 import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
+import javafx.scene.text.Font;
 import javafx.scene.Scene;
 import javafx.stage.Stage;
 
@@ -9,6 +10,19 @@ public class MainApp extends Application {
 
     @Override
     public void start(Stage primaryStage) throws Exception {
+
+        // Registar todas as variações da JetBrains Mono
+        String[] fontes = {
+                "JetBrainsMono-Bold.ttf",
+                "JetBrainsMono-Regular.ttf",
+                "JetBrainsMono-ExtraBold.ttf"
+        };
+
+        for (String fonte : fontes) {
+            Font.loadFont(getClass().getResource("/fonts/" + fonte).toExternalForm(), 14);
+        }
+
+
         FXMLLoader fxmlLoader = new FXMLLoader(getClass().getResource("/com.example.gestaooleos/view/contratos-view.fxml"));
         Scene scene = new Scene(fxmlLoader.load(), 600, 400);
         primaryStage.setTitle("Gestão de Óleos");
